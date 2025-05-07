cb8687238ed948b16aa6ea30e8741341e9c7cee5
Jared Sauve <sauvejared@gmail.com>
2025-05-07 18:16:44 -0400

    Add project files.

---
+++ Leetcode/GoogleSolutions.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode;
+
+public class GoogleSolutions
+{
+    public int NumUniqueEmails(string[] emails)
+    {
+        HashSet<string> reducedEmails = new HashSet<string>();
+        foreach(var email in emails)
+        {
+            var split = email.Split('@');
+            var local = split[0];
+            var domain = split[1];
+
+            var separatePlus = local.Split('+');
+            var reducedLocal = separatePlus[0];
+
+            reducedLocal = reducedLocal.Replace(".", "");
+
+            var reducedEmail = $"{reducedLocal}@{domain}";
+
+            if (reducedEmails.Contains(reducedEmail))
+            {
+                continue;
+            }
+
+            reducedEmails.Add(reducedEmail);
+        }
+
+        return reducedEmails.Count();
+    }
+
+    //get maximum value and always get closer by removing the lower number from the list
+    //Difficulty: Med
+    public int ContainerWithMostWater(int[] height)
+    {
+        int maxNumber = 0;
+        int i = 0;
+        int j = height.Length - 1;
+        while (i < j)
+        {
+            int distance = j - i;
+            int compareValue;
+            if (height[i] > height[j])
+            {
+                compareValue = height[j];
+                j--;
+            }
+            else
+            {
+                compareValue = height[i];
+                i++;
+            }
+
+            var result = compareValue * distance;
+
+            if(maxNumber < result)
+            {
+                maxNumber = result;
+            }
+        }
+
+        return maxNumber;
+    }
+
+    //sliding window move to the end and track the biggest gap streak
+    //Difficulty: Medium
+    public int MaxDistToClosestPerson(int[] seats)
+    {
+        int maxDistance = 0;
+        int currentDistance = 0;
+        int i = 0;
+        while (i < seats.Length)
+        {
+            if (seats[i] == 1)
+            {
+                if ((i - currentDistance) == 0 && i != 0)
+                {
+                    maxDistance = maxDistance > currentDistance ? maxDistance : currentDistance;
+                }
+                else
+                {
+                    var calc = (currentDistance + 1) / 2;
+                    maxDistance = maxDistance > calc ? maxDistance : calc;
+                }
+
+                currentDistance = 0;
+            }
+            else
+            {
+                currentDistance++;
+            }
+
+            i++;
+        }
+
+        return maxDistance > currentDistance ? maxDistance : currentDistance;
+    }
+
+    //runner pattern
+    //unable to complete
+    //Difficulty: Hard
+    //probably need to look up heap and priority queue's
+    public double MincostToHireWorkers(int[] quality, int[] wage, int k)
+    {
+        var minTotal = int.MaxValue;
+
+
+        throw new NotImplementedException();
+    }
+
+    //Difficulty: Med
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode? First = l1;
+        ListNode? Second = l2;
+        ListNode Head = new ListNode();
+        ListNode? Result = null;
+
+        var carryOver = 0;
+
+        while (First is not null || Second is not null || carryOver != 0)
+        {
+            if(Result is null)
+            {
+                Result = Head;
+            }
+            else
+            {
+                Result.next = new ListNode();
+                Result = Result.next;
+            }
+
+            var total = (First?.val ?? 0) + (Second?.val ?? 0) + carryOver;
+
+            carryOver = total / 10;
+
+            var result = total % 10;
+
+            Result.val = result;
+
+            First = First?.next;
+            Second = Second?.next;
+        }
+
+        return Head;
+    }
+
+    public int NumIslands(char[][] grid)
+    {
+
+
+        throw new NotImplementedException();
+    }
+}
+
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public ListNode createList(List<int> values)
+    {
+        var Head = new ListNode();
+        ListNode? Current = null;
+        foreach(var value in values)
+        {
+            if (Current is null)
+            {
+                Current = Head;
+            }
+            else
+            {
+                Current.next = new ListNode();
+                Current = Current.next;
+            }
+
+            Current.val = value;
+        }
+
+        return Head;
+    }
+}
+
---
+++ Leetcode/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode;
+
+var google = new GoogleSolutions();
+
+//********************  Arrays *********************************
+
+//string[] emails = ["test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"];
+
+//int[] height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+
+//int[] seats = [1, 0, 0, 0, 1, 0, 1];
+
+//(int[] quality, int[] wage, int k) workers = ([10, 20, 5], [70, 50, 30], 2);
+
+//******************** End Arrays *********************************
+
+
+//********************  Linked Lists *********************************
+
+//var x = new ListNode().createList([9, 9, 9, 9, 9, 9, 9]);
+//var y = new ListNode().createList([9, 9, 9, 9]);
+//var x = new ListNode().createList([2, 4, 3]);
+//var y = new ListNode().createList([5, 6, 4]);
+
+
+//var nodes = google.AddTwoNumbers(x, y);
+
+//while (nodes is not null)
+//{
+//    Console.WriteLine(nodes.val);
+//    nodes = nodes.next;
+//}
+
+//******************** End Linked Lists *********************************
+
+//********************  Trees / Graphs *********************************
+
+char[][] grid = [
+  ['1','1','1','1','0'],
+  ['1','1','0','1','0'],
+  ['1','1','0','0','0'],
+  ['0','0','0','0','0']
+];
+
+Console.WriteLine(google.NumIslands(grid));
+
+//********************  End Trees / Graphs *********************************
+
