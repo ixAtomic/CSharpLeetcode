ac88403ef7d1813a9a20a1154a52bc8505915d9b
Jared Sauve <sauvejared@gmail.com>
2025-07-19 11:54:05 -0400

    MinimumSpanningTree Kruskals Algo

--- Leetcode/Graph/MinimumSpanningTree/MinCstToCnctAllPts.cs
+++ Leetcode/Graph/MinimumSpanningTree/MinCstToCnctAllPts.cs
@@ -8,5 +8,46 @@ namespace LeetCode.Graph.MinimumSpanningTree;
 
 public class MinCstToCnctAllPts
 {
+    public int MinCostConnectPoints(int[][] points)
+    {
+        if(points == null || points.Length == 0) return 0;
 
+        PriorityQueue<(int from, int to, int distance), int> edges = new PriorityQueue<(int from, int to, int distance), int>();
+
+        var edgeSize = points.Length - 1;
+        DisjointSet DS = new DisjointSet(points.Length);
+        for (int i = 0; i < points.Length; i++) 
+        {
+            var xi = points[i][0];
+            var yi = points[i][1];
+
+            for (int j = i + 1; j < points.Length; j++) 
+            { 
+                var xj = points[j][0];
+                var yj = points[j][1];
+
+                var x = Convert.ToInt32(Math.Abs(xi - xj));
+                var y = Convert.ToInt32(Math.Abs(yi - yj));
+
+                var distance = x + y;
+
+                edges.Enqueue((i, j, distance), distance);
+            }
+        }
+
+        var total = 0;
+        while (edges.Count > 0 && edgeSize > 0) 
+        {
+            var current = edges.Dequeue();
+
+            if(!DS.IsConnected(current.from, current.to))
+            {
+                DS.Union(current.from, current.to);
+                total += current.distance;
+                edgeSize--;
+            }
+        }
+
+        return total;
+    }
 }
---
+++ LeetcodeTestProject/MinCstToCnctAllPtsTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using LeetCode.Graph.MinimumSpanningTree;
+
+public class MinCstToCnctAllPtsTests
+{
+    [Fact]
+    public void MinCostConnectPoints_ExampleCase_ReturnsExpected()
+    {
+        var points = new int[][]
+        {
+            new int[] { 0, 0 },
+            new int[] { 2, 2 },
+            new int[] { 3, 10 },
+            new int[] { 5, 2 },
+            new int[] { 7, 0 }
+        };
+
+        var solver = new MinCstToCnctAllPts();
+        int result = solver.MinCostConnectPoints(points);
+
+        // Expected output for this input is 20 (LeetCode standard)
+        Assert.Equal(20, result);
+    }
+}
