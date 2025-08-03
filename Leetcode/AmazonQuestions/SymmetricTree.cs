89649295be56d5fec25dedd4421795230c6a32a6
Jared Sauve <sauvejared@gmail.com>
2025-08-03 16:41:54 -0400

    struggling to get things right

---
+++ Leetcode/AmazonQuestions/SymmetricTree.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.AmazonQuestions;
+
+public class SymmetricTree
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        Queue<TreeNode> nodes = new Queue<TreeNode>();
+        nodes.Enqueue(root);
+
+        while (nodes.Any()) 
+        { 
+            int count = nodes.Count;
+            List<TreeNode?> level = new List<TreeNode?>();
+
+            for(int i = 0; i < count; i++)
+            {
+                var current = nodes.Dequeue();
+                level.Add(current?.left ?? null);
+                level.Add(current?.right ?? null);
+
+                if(current!.left is not null)
+                {
+                    nodes.Enqueue(current.left);
+                }    
+                
+                if(current!.right is not null)
+                {
+                    nodes.Enqueue(current.right);
+                }
+            }
+
+            int m = 0;
+            int n = level.Count - 1;
+
+            while (m < n)
+            {
+                var left = level.ElementAt(m);
+                var right = level.ElementAt(n);
+
+                if(left?.val != right?.val)
+                {
+                    return false;
+                }
+
+                m++;
+                n--;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ LeetcodeTestProject/AmazonQuestionsTests/SymmetricTreeTests.cs
@@ -0,0 +1,75 @@
+﻿using Xunit;
+using LeetCode.AmazonQuestions;
+
+namespace LeetcodeTestProject.AmazonQuestionsTests;
+public class SymmetricTreeTests
+{
+    [Fact]
+    public void IsSymmetric_Input_1_2_2_3_4_4_3_ReturnsTrue()
+    {
+        // Arrange: Build the tree [1,2,2,3,4,4,3]
+        var root = new TreeNode(1,
+            new TreeNode(2,
+                new TreeNode(3),
+                new TreeNode(4)
+            ),
+            new TreeNode(2,
+                new TreeNode(4),
+                new TreeNode(3)
+            )
+        );
+        var solver = new SymmetricTree();
+
+        // Act
+        var result = solver.IsSymmetric(root);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsSymmetric_Input_1_2_2_null_3_null_3_ReturnsFalse()
+    {
+        // Arrange: Build the tree [1,2,2,null,3,null,3]
+        var root = new TreeNode(1,
+            new TreeNode(2,
+                null,
+                new TreeNode(3)
+            ),
+            new TreeNode(2,
+                null,
+                new TreeNode(3)
+            )
+        );
+        var solver = new SymmetricTree();
+
+        // Act
+        var result = solver.IsSymmetric(root);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void IsSymmetric_Input_1_2_2_null_3_3_null_null_4_null_4_ReturnsFalse()
+    {
+        // Arrange: Build the tree [1, 2, 2, null, 3, 3, null, null, 4, null, 4]
+        var root = new TreeNode(1,
+            new TreeNode(2,
+                null,
+                new TreeNode(3, null, new TreeNode(4))
+            ),
+            new TreeNode(2,
+                new TreeNode(3, null, new TreeNode(4)),
+                null
+            )
+        );
+        var solver = new SymmetricTree();
+
+        // Act
+        var result = solver.IsSymmetric(root);
+
+        // Assert
+        Assert.False(result);
+    }
+}
