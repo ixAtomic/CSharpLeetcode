09b20fc53ef45f8f34ac3e55ed11b6ead3c2b497
ixatomic <sauvejared@gmail.com>
2025-10-14 23:14:42 -0400

    Add EvaluateRPN and MinStack implementations
    Added the `EvaluateReversePolishNotation` class to evaluate Reverse Polish Notation expressions using a stack-based approach. Implemented the `EvalRPN` method to parse tokens, perform arithmetic operations, and return the result.
    
    Added the `MinStack` class to implement a stack with `Push`, `Pop`, `Top`, and `GetMin` operations. Utilized two stacks to track values and the current minimum efficiently.

---
+++ Leetcode/LeetCodeTop150/Stack/EvaluateReversePolishNotation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetCodeTop150.Stack;
+
+public class EvaluateReversePolishNotation
+{
+    List<string> operations = ["+", "-", "*", "/"];
+    public int EvalRPN(string[] tokens)
+    {
+        Stack<int> values = new Stack<int>();
+        foreach (var item in tokens)
+        {
+            if (operations.Contains(item))
+            {
+                int second = values.Pop();
+                int first = values.Pop();
+                switch (item)
+                {
+                    case "+":
+                        values.Push(first + second);
+                        break;
+                    case "-":
+                        values.Push(first - second);
+                        break;
+                    case "*":
+                        values.Push(first * second);
+                        break;
+                    case "/":
+                        values.Push(first / second);
+                        break;
+                }
+            }
+            else
+            {
+                values.Push(int.Parse(item));
+            }
+        }
+
+        return values.Pop();
+    }
+}
---
+++ Leetcode/LeetCodeTop150/Stack/MinStack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LeetCodeTop150.Stack;
+
+//[4, 3, 2]
+//[0, 1, 2]
+
+public class MinStack
+{
+    private Stack<int> _stack { get; set; }
+    private Stack<(int index, int value)> _lowestIndex { get; set; }
+
+    public MinStack()
+    {
+        _stack = new Stack<int>();
+        _lowestIndex = new Stack<(int index, int value)>();
+    }
+
+    public void Push(int val)
+    {
+        if(_lowestIndex.Count == 0 || val < _lowestIndex.Peek().value)
+        {
+            _lowestIndex.Push((_stack.Count, val));
+        }
+
+        _stack.Push(val);
+    }
+
+    public void Pop()
+    {
+        _stack.Pop();
+
+        if(_stack.Count - 1 < _lowestIndex.Peek().index)
+        {
+            _lowestIndex.Pop();
+        }
+    }
+
+    public int Top()
+    {
+        return _stack.Peek();
+    }
+
+    public int GetMin()
+    {
+        return _lowestIndex.Peek().value;
+    }
+}
